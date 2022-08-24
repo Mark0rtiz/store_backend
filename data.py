260d3b9620173ce43ce0ddc31abc35880f526150
Mark0rtiz <106716676+Mark0rtiz@users.noreply.github.com>
2022-08-23 19:42:29 -0700

    store_backend

--- data.py
+++ data.py
@@ -9,3 +9,77 @@ me = {
         "city": "San Diego",
     }
 }
+
+
+catalog = [
+    {
+    "_id": "1",
+    "title": "Sony a6600",
+    "price": 1399.0,
+    "category": "photo & video",
+    "image": "a6600.jpeg"
+  },
+  {
+    "_id": "2",
+    "title": "Panasonic Lumix G100",
+    "price": 799.99,
+    "category": "photo & video",
+    "image": "Lumixg100.jpeg"
+  },
+  {
+    "_id": "3",
+    "title": "Canon EOS RP",
+    "price": 1299.99,
+    "category": "photo & video",
+    "image": "EOSRP.jpeg"
+  },
+  {
+    "_id": "4",
+    "title": "Xbox Series X",
+    "price": 499.99,
+    "category": "video games",
+    "image": "Xbox.jpeg"
+  },
+  {
+    "_id": "5",
+    "title": "PlayStation 5",
+    "price": 499.99,
+    "category": "video games",
+    "image": "PS5.jpeg"
+  },
+  {
+    "_id": "6",
+    "title": "Nintendo Switch OLED",
+    "price": 349.99,
+    "category": "video games",
+    "image": "Switch.jpeg"
+  },
+  {
+    "_id": "7",
+    "title": "",
+    "price": 49.99,
+    "category": "computer",
+    "image": ""
+  },
+  {
+    "_id": "8",
+    "title": "",
+    "price": 49.99,
+    "category": "computer",
+    "image": ""
+  },
+  {
+    "_id": "9",
+    "title": "",
+    "price": 49.99,
+    "category": "computer",
+    "image": ""
+  },
+  {
+    "_id": "10",
+    "title": "",
+    "price": 49.99,
+    "category": "computer",
+    "image": ""
+  }
+]
--- server.py
+++ server.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, request, abort
 import json
+import random
 from data import me
+from data import me, catalog 
 
 app = Flask(__name__)
 
@@ -35,4 +37,111 @@ def test_api():
 def about_api():
     return json.dumps(me)
 
-app.run(debug=True)
+@app.get("/api/catalog")
+def get_catalog():
+    #return list of products
+    return json.dumps(catalog)
+
+@app.post("/api/catalog")
+def save_product():
+    product = request.get_json()
+    if not "title" in product:
+        return abort(400, "ERROR: Title is required")
+    if len(product["title"]) < 5:
+        return abort(400, "ERROR: Title must be greater than 5 characters")
+    if not "price" in product:
+        return abort(400, "ERROR: Price is required")
+    if product["price"] < 1:
+        return abort(400, "ERROR: Price must be $1 or greater")
+    
+    product["_id"] = random.randint(100, 100000)
+    catalog.append(product)
+
+    return product
+
+@app.get("/api/product/<id>")
+def get_product_by_id(id):
+    for prod in catalog:
+        if prod["_id"] == id:
+            return json.dumps(prod)
+    return json.dumps("Error: id not found")
+
+@app.get("/api/products/<category>")
+def get_products_in_category(category):
+    results = []
+    for prod in catalog:
+        if prod["category"].lower() == category.lower():
+            results.append(prod)
+    return json.dumps(results)
+
+@app.get("/api/count")
+def count_api():
+    count = len(catalog)
+    return json.dumps(count)
+    
+@app.get("/api/catalog/total")
+def catalog_total():
+    total = 0
+    for prod in catalog:
+        total += prod["price"]
+    return json.dumps(total)
+
+@app.get("/api/catalog/cheapest")
+def lowest_price():
+    cheapest = catalog[0]
+    for prod in catalog:
+        if prod["price"] < cheapest["price"]:
+            cheapest = prod
+    return json.dumps(cheapest) 
+        
+
+
+# app.run(debug=True)
+
+
+
+@app. get("/api/game/<pick>")
+def game (pick):
+
+    num = random.randint(0, 2)
+    pc = ""
+    if num == 0:
+        pc = "paper"
+    elif num == 1:
+        pc = "rock"
+    else:
+        pc = "scissors"
+
+    winner = ""
+    if pick == "paper":
+        if pc == "rock":
+            winner = "you"
+        elif pc == "scissors":
+            winner = "pc"
+        else:
+            winner = "draw"
+
+    elif pick == "rock":
+        if pc == "rock":
+            winner = "draw"
+        elif pc == "scissors":
+            winner = "you"
+        else:
+            winner = "pc"
+
+    elif pick == "scissors":
+        if pc == "rock":
+            winner = "pc"
+        elif pc == "scissors":
+            winner = "draw"
+        else:
+            winner = "you"
+
+
+    results = {
+        "you": pick,
+        "pc": pc,
+        "winner": winner
+    }
+
+    return json.dumps(results)
